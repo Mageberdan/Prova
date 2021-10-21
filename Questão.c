9f76ba195e302236eaff24f60332967b80c1ca54
Mageberdan <92955981+Mageberdan@users.noreply.github.com>
2021-10-21 18:58:21 -0300

    Create Questão.c

---
+++ Questão.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#define n 50 
+
+void ordena_str(S[n]){
+    
+}
+
+int c = 0;
+int main(){
+    printf("Escreva 10 Strings");
+    for (c < 11);
+    
+    return 0;
+}
